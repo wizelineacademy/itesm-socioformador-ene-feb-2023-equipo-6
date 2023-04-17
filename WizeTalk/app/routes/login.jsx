33e1e9c070e0c7d84afa4b28c4cad90aa122645f
Edgar Alexandro Castillo Palacios <a00830568@tec.mx>
2023-04-17 08:00:29 -0600

    Added Webcam recorder component
    Added Webcam recorder component to the routes folder.

--- WizeTalk/app/routes/login.jsx
+++ WizeTalk/app/routes/login.jsx
@@ -1,4 +1,5 @@
 
+import { Outlet } from '@remix-run/react';
 import wizetalk from '../wizetalk.png';
 import LoginForm from "~/components/evaluation/LoginForm";
 import WizelineHeader from '~/components/WizelineHeader';
@@ -8,11 +9,11 @@ export default function LoginPage() {
     return (
         <main>
             <WizelineHeader />
-            <section class="flex min-h-screen bg-gradient-to-r from-white via-white via-60% to-wizeblue-100 to-60%">
-            <container class="flex flex-row m-auto space-x-40">
+            <section className="flex min-h-screen bg-gradient-to-r from-white via-white via-60% to-wizeblue-100 to-60%">
+            <container className="flex flex-row m-auto space-x-40">
                 <LoginForm />
-                <div class="flex items-center">
-                    <div class="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow">
+                <div className="flex items-center">
+                    <div className="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow">
                     <img src={wizetalk} />
                     </div>
                 </div>
