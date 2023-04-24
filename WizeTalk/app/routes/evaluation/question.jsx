d63d7f4b3d022485d113fb99e08aedcc5d31d187
SimplySanti <A01620647@tec.mx>
2023-04-24 12:50:19 -0600

    Added feature to play the currect question based on file path.
    Added voices to the question page and linked evaluation pages.

--- WizeTalk/app/routes/evaluation/question.jsx
+++ WizeTalk/app/routes/evaluation/question.jsx
@@ -1,23 +1,38 @@
-/* import { FiVolume2 } from "react-icons/fi"
-import { BiTime } from "react-icons/bi" */
 import { BsPlay, BsFillPlayFill } from "react-icons/bs";
-import { Link } from "@remix-run/react";
+import { useLoaderData, useNavigate } from "@remix-run/react";
 import VideoShow from '~/components/evaluation/VideoShow';
-
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
-import audio from '../../../public/Chill.wav'
-
+import { getQuestions } from "../../data/questions.server";
 
 export default function QuestionPage() {
 
+    const questions = useLoaderData();
+
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [isPlaying, setIsPlaying] = useState(false);
     const [seconds, setSeconds] = useState();
+    const [audio, setAudio] = useState(questions[0].audio_path)
+
     const [play, { pause, duration, sound }] = useSound(audio);
+
+    const navigate = useNavigate();
+
+    const handleNextQuestionClick = () => {
+        const nextQuestionIndex = currentQuestionIndex + 1;
+        setAudio(questions[nextQuestionIndex].audio_path);
+        setIsPlaying(false);
+        setCurrentQuestionIndex(nextQuestionIndex);
+        setSeconds(0);
+    }
 
     function playBtn() {
         setIsPlaying(true);
         play();
+    }
+
+    const endTest = () => {
+        navigate('/evaluation/results');
     }
 
     useEffect(() => {
@@ -25,7 +40,7 @@ export default function QuestionPage() {
             if (sound) {
                 setSeconds(sound.seek([]));
             }
-        }, 1000);
+        }, 500);
         return () => clearInterval(interval);
     }, [sound]);
 
@@ -34,14 +49,8 @@ export default function QuestionPage() {
             <section>
                 <div className="flex justify-center mx-[20%] py-5">
                     <h1 className="text-center font-sans text-lg font-bold">
-                        Q1.    For this question, listen to the audio and once it has finished answer what has been asked.
+                        Q{currentQuestionIndex + 1}. {questions[currentQuestionIndex].instructions}
                     </h1>
-                    {/* <div className="flex">
-                        <FiVolume2 />
-                        <BiTime /><p>
-                            14:01
-                        </p>
-                    </div> */}
                 </div>
                 <div className="flex justify-center drop-shadow-xl h-16 bg-white mx-[35%] items-center rounded-lg px-5">
                     {!isPlaying ?
@@ -49,23 +58,31 @@ export default function QuestionPage() {
                             <BsFillPlayFill size={40} color="#43C2FF" />
                         </button>)
                         : (
-                            <BsPlay size={50} color="#43C2FF" />
+                            <BsPlay size={40} color="#43C2FF" />
                         )}
-                    <input type="range" value={seconds} defaultValue={0} disabled="true" className="w-full" />
+                    <input type="range" value={seconds} disabled={true} max={Math.ceil((duration/1000))}/>
                 </div>
                 <div className="h-[30%] flex justify-center mx-[20%] my-5 rounded-md">                    <VideoShow />
-                            <img src="https://elcomercio.pe/resizer/Ypm41sg9TDq9RJ9se2RDUKu5whY=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KOYACOEIZ5E4HN4TFJXQNTD4QM.jpg"/>
+                    <img src="https://elcomercio.pe/resizer/Ypm41sg9TDq9RJ9se2RDUKu5whY=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KOYACOEIZ5E4HN4TFJXQNTD4QM.jpg" />
                 </div>
                 <div className="flex justify-center m-20">
                     <div className="inline-flex items-center px-4 py-2 mr-3 text-sm font-medium text-white bg-wizeblue-100 border border-gray-300 rounded-lg">
-                        1 / 10
+                        {currentQuestionIndex + 1} / 10
                     </div>
-                    <Link className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700">
+                    {questions[currentQuestionIndex + 1] !== undefined ? 
+                        <button onClick={() => handleNextQuestionClick()} className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700">
                         Next
                         <svg aria-hidden="true" className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </Link>
+                        </button> :
+                        <button onClick={() => endTest()} className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700">End Test</button>
+                        }
                 </div>
             </section >
         </>
     )
+}
+
+export async function loader() {
+    const questions = await getQuestions();
+    return questions;
 }
