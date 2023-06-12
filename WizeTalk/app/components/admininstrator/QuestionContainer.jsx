d6f1e1f44bd4adac3a343389e8db9b1d03b4b2d3
Hugo Palomares <80979871+occon@users.noreply.github.com>
2023-06-12 13:28:01 -0600

    Added filter for admin to view questions

--- WizeTalk/app/components/admininstrator/QuestionContainer.jsx
+++ WizeTalk/app/components/admininstrator/QuestionContainer.jsx
@@ -1,23 +1,55 @@
 import { useLoaderData } from '@remix-run/react';
 import QuestionItem from './QuestionItem';
 import { Link } from '@remix-run/react';
+import { questionFilter } from '../../routes/admin.questions';
+import { useState } from 'react';
 
 export default function QuestionContainer() {
+    let hasQuestions;
+    const q = useLoaderData();
+    const [questions, setQuestions] = useState(q);
 
-    const questions = useLoaderData();
-    const hasQuestions = questions && questions.length > 0;
+    async function getFilteredQuestions(){
+        const category = document.getElementById("underline_select").value;
+        const filter = await questionFilter(category);
+        setQuestions(filter);
+    }
 
+    // const getQuestions = async () => {
+    //     const data = {
+    //         option: 'english',
+    //     }
+    //     console.log(data);
+    //     await fetch("/questionfilter", {
+    //         method: "POST",
+    //         body: JSON.stringify(data),
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //     });
+    // }
+    // const questions = getQuestions();
+    // console.log(questions);
+    var categoryQuestions;
+    // questions.forEach(element => {
+    //     if (element.categoria == document.getElementById("underline_select").value){
+    //         categoryQuestions.push(element);
+    //     }
+    // });
+    const hq = questions && questions.length > 0
+    hasQuestions = hq;
+    // setHasQuestions(hq);
     return (
         <>
-            <div className="flex flex-col border-2 rounded-md border-black p-8 gap-5 bg-white">
+            <div className="flex flex-col border-2 rounded-md border-black m-3.5 p-8 gap-5 bg-white">
                 <div>
                     <label htmlFor="underline_select" className="sr-only">Underline select</label>
-                    <select id="underline_select" className="block py-2.5 px-0 w-full text-sm text-black-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+                    <select onChange = {getFilteredQuestions} id="underline_select" className="block py-2.5 px-0 w-full text-sm text-black-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer">
                         <option defaultValue>Choose a category</option>
-                        <option value="English">English</option>
-                        <option value="Front-End">Front-End</option>
-                        <option value="Back-End">Back-End</option>
-                        <option value="QA">Quality Assurance</option>
+                        <option value="english">English</option>
+                        <option value="frontend">Front-End</option>
+                        <option value="backend">Back-End</option>
+                        <option value="fullstack">Full-Stack</option>
                     </select>
                 </div>
                 <div>
